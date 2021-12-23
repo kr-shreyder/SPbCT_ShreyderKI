f0555775daf345bd665b5aa17ce80590b1c9dcdc
shreyder.ki <92268472+kr-shreyder@users.noreply.github.com>
2021-12-23 20:20:37 +0300

    Delete EdPract directory

--- EdPract/Auth.cpp
+++
@@ -1,9 +0,0 @@
-#include "Auth.h"
-#include <Windows.h>
-using namespace EdPract;
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew Auth);
-	return 0;
-}
--- EdPract/Prog.cpp
+++
@@ -1,2 +0,0 @@
-#include "Prog.h"
-
